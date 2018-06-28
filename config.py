1d02ba52d0ba519f0f8c748e0cc9003de0feb165
Silent777 <silentismyfriend@gmail.com>
2018-06-28 14:12:30 +0300

    set up database

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'sum41182',
+    'db': 'shopping',
+    'host': 'localhost',
+    'port': '5432',
+}
 
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'any secret word'
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
 
 class ProductionConfig(Config):
